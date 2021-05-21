e8a646d4baec9d2bc4f422a16c925f6094e8cb5e
EgorKiryachkov <egor00015@gmail.com>
2021-05-21 09:28:43 +0300

    Update1

--- PythonTkinter.py
+++ PythonTkinter.py
@@ -1,16 +1,13 @@
 from tkinter import *
 
+from tkinter import scrolledtext
+
 list_ = ["Т-26","БТ-7","Т-70","Т-28","КВ-1","КВ-2","Т-34","Т-34-85","ИС-2"]
 foto_list=["t26.gif","bt7.gif","t70.gif","t28.gif","kv1.gif","kv2.gif","t34_1941.gif","t34_85.gif","is2.gif"]
-global can, pc
 def list_to_txt(event):
-    global can,pc#
     txt.delete(0.0,END)
     valik=lbox.curselection()
     txt.insert(END,lbox.get(valik[0]))
-    can.delete(ALL)#
-    foto=PhotoImage(file=foto_list[valik[0]])#
-    can.create_image(0,0,image=foto,anchor=NW)#
 def txt_to_list(event):
     text=txt.get(0.0,END)
     text=text[-2:-1]
@@ -24,26 +21,53 @@ def txt_to_list(event):
         txt.delete(0.0,END)
 
 def description():
-    text=txt.get(0.0,END)
-    print(list(text))
-    if text=="t26.gif\n":
-        t26=open("t26.txt", encoding="utf-8-sig")
-    elif text=="bt7.gif\n":
-        bt7=open("bt7.txt", encoding="utf-8-sig")
-    elif text=="t70.gif\n":
-        t70=open("t70.txt", encoding="utf-8-sig")
-    elif text=="t28.gif\n":
-        t28=open("t28.txt", encoding="utf-8-sig")
-    elif text=="kv1.gif\n":
-        kv1=open("kv1.txt", encoding="utf-8-sig")
-    elif text=="kv2.gif\n":
-        kv2=open("kv2.txt", encoding="utf-8-sig")
-    elif text=="t34_1941.gif\n":
-        t34_1941=open("t34_1941.txt", encoding="utf-8-sig")
-    elif text=="t34_85.gif\n":
-        t34_85=open("t34_85.txt", encoding="utf-8-sig")
-    elif text=="is2.gif\n":
-        is2=open("is2.txt", encoding="utf-8-sig")
+    desc.delete(1.0, END)
+    show.config(text=lbox.get(ANCHOR))
+    if lbox.get(ANCHOR)=="Т-26":
+        t=open("t26.txt", encoding="utf-8-sig")
+        f=t.read()
+        desc.insert(INSERT,f)
+        t.close()
+    elif lbox.get(ANCHOR)=="БТ-7":
+        t=open("bt7.txt", encoding="utf-8-sig")
+        f=t.read()
+        desc.insert(INSERT,f)
+        t.close()
+    elif lbox.get(ANCHOR)=="Т-70":
+        t=open("t70.txt", encoding="utf-8-sig")
+        f=t.read()
+        desc.insert(INSERT,f)
+        t.close()
+    elif lbox.get(ANCHOR)=="Т-28":
+        t=open("t28.txt", encoding="utf-8-sig")
+        f=t.read()
+        desc.insert(INSERT,f)
+        t.close()
+    elif lbox.get(ANCHOR)=="КВ-1":
+        t=open("kv1.txt", encoding="utf-8-sig")
+        f=t.read()
+        desc.insert(INSERT,f)
+        t.close()
+    elif lbox.get(ANCHOR)=="КВ-2":
+        t=open("kv2.txt", encoding="utf-8-sig")
+        f=t.read()
+        desc.insert(INSERT,f)
+        t.close()
+    elif lbox.get(ANCHOR)=="Т-34":
+        t=open("t34_1941.txt", encoding="utf-8-sig")
+        f=t.read()
+        desc.insert(INSERT,f)
+        t.close()
+    elif lbox.get(ANCHOR)=="Т-34-85":
+        t=open("t34_85.txt", encoding="utf-8-sig")
+        f=t.read()
+        desc.insert(INSERT,f)
+        t.close()
+    elif lbox.get(ANCHOR)=="ИС-2":
+        t=open("is2.txt", encoding="utf-8-sig")
+        f=t.read()
+        desc.insert(INSERT,f)
+        t.close()
 
 aken=Tk()
 aken.title("Самые знаменитые танки времён ВОВ")
@@ -66,10 +90,13 @@ lbox.bind("<<ListboxSelect>>",list_to_txt)
 txt=Text(aken,height=1,width=20,wrap=WORD)
 txt.grid(row=0,column=2)
 txt.bind("<Return>",txt_to_list)
-pc=PhotoImage(file="")
-panel = Label(aken,image=pc)
-panel.grid(row=2,column=3,)
+#pc=PhotoImage(file=img)
+#panel = Label(aken,image=pc)
+#panel.grid(row=2,column=3,)
 bt=Button(text='Информация', command=description).grid(row=1,column=0)
-desc=Label(aken, text="Описание", width=40, height=40)
+desc=scrolledtext.ScrolledText(aken, width=70, height=10, borderwidth=5)
 desc.grid(row=3,column=3)
+
+show = Label(aken)
+
 aken.mainloop()
